727ebbcf6b9d0a5c31f18b01077de7a29473dac7
wangqinglu-on-macos <18353184289@163.com>
2018-05-09 17:15:23 +0800

    1

---
+++ my_ds.h
@@ -0,0 +1,24 @@
+#ifndef _DS_H_
+#define _DS_H_
+//用户信息结构体
+typedef struct userlist
+{
+	char name[20];	//用户名
+	char host[20];	//主机名
+	int s_addr;		//IP地址(32位网络字节序)
+	struct userlist *next;
+}IPMSG_USER;
+
+//文件信息结构体
+typedef struct filelist
+{
+	char name[50];//文件名
+	int num;// 文件序号
+	long pkgnum;//包编号
+	long size;//文件大小
+	long ltime;//最后修改时间
+	char user[10];	//发送者用户名
+	struct filelist *next;
+}IPMSG_FILE;
+
+#endif
