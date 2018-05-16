14d4daac1faf9695eb7cf5436142d01785c45497
wql <18353184289@163.com>
2018-05-16 14:23:40 +0800

    1

--- main.c
+++ main.c
@@ -1,9 +1,36 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <unistd.h> 
 #include "./my_udp/my_udp.h"
 
+void test1(){
+    int i;
+    for(i = 0; i < 3; i++){
+        printf("one test %d",i);
+        sleep(1);
+    }
+}
+void test2(){
+    int i;
+    for(i = 0; i < 3; i++){
+        printf("two test %d",i);
+        sleep(1);
+    }
+}
 int main(int argc,char* argv[]){
-    br_entry_send();
-    br_exit_send();
+    //br_entry_send();
+    //br_exit_send();
+    int ret;
+    pthread_t id1,id2;
+    ret = pthread_create(&id1,NULL,(void*)test1,NULL);
+    if(ret){
+        perror("id1 created failed!");
+    }
+    ret = pthread_create(&id2,NULL,(void*)test2,NULL);
+    if(ret){
+        perror("id2 created failed!");
+    }
+    pthread_join(id1,NULL);
+    pthread_join(id2,NULL);
     return 0;
 }    
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
 myclient:
-	$(CC) ./my_tcp/tcp_op.c ./my_udp/udp_op.c ./util/util.c main.c -o myclient
+	$(CC) ./my_tcp/tcp_op.c ./my_udp/udp_op.c ./util/util.c main.c -o myclient -lpthread
 clean:
 	rm -rf ./myclient
