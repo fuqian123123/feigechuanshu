0105681882de71795ffd8afe6b20f124a9aa641e
wql <18353184289@163.com>
2018-05-16 13:57:05 +0800

    1

--- my_udp/udp_op.c
+++ my_udp/udp_op.c
@@ -4,15 +4,15 @@
 #include <sys/socket.h>
 #include <time.h>
 #include <string.h>
-#include<netinet/in.h>
+#include <netinet/in.h>
 #include "../util/util.h"
 #include "../ipmsg.h"
 #include "../my_udp/my_udp.h"
 
+static const char BR_ADDR[] = "10.18.23.255";
 static const int BR_PORT = 4001;
 static const int RECV_PORT = 4002;
 static char buffer[1024];
-static char myHostName[256];
 void br_entry_send(){
     int brFd;
     if((brFd = socket(AF_INET,SOCK_DGRAM,0)) == -1){
@@ -23,15 +23,41 @@ void br_entry_send(){
     struct sockaddr_in theirAddr;
     memset(&theirAddr,0,sizeof(struct sockaddr_in));
     theirAddr.sin_family = AF_INET;
-    theirAddr.sin_addr.s_addr = inet_addr("10.18.23.255");
+    theirAddr.sin_addr.s_addr = inet_addr(BR_ADDR);
     theirAddr.sin_port = htons(BR_PORT);
     int sendBytes;
+    char myHostName[256];
     gethostname(myHostName,sizeof(myHostName));
 
     sprintf(buffer,"%d:%ld:%s:%s:%d:%s",IPMSG_VERSION,time(NULL),myHostName,myHostName,IPMSG_BR_ENTRY,"");
     if((sendBytes = sendto(brFd,buffer,strlen(buffer),0,
             (struct sockaddr*)&theirAddr,sizeof(struct sockaddr)))== -1){
         perror("br_entry:udp send msg failed!");
+	    return EXIT_FAILURE;
+    }
+    printf("%d\n",sendBytes);
+    close(brFd);
+}
+void br_exit_send(){
+    int brFd;
+    if((brFd = socket(AF_INET,SOCK_DGRAM,0)) == -1){
+        perror("br_exit:udp socket create failed!");
+    }
+    int optval = 1;
+    setsockopt(brFd,SOL_SOCKET,SO_BROADCAST | SO_REUSEADDR,&optval,sizeof(int));
+    struct sockaddr_in theirAddr;
+    memset(&theirAddr,0,sizeof(struct sockaddr_in));
+    theirAddr.sin_family = AF_INET;
+    theirAddr.sin_addr.s_addr = inet_addr(BR_ADDR);
+    theirAddr.sin_port = htons(BR_PORT);
+    int sendBytes;
+    char myHostName[256];
+    gethostname(myHostName,sizeof(myHostName));
+
+    sprintf(buffer,"%d:%ld:%s:%s:%d:%s",IPMSG_VERSION,time(NULL),myHostName,myHostName,IPMSG_BR_EXIT,"");
+    if((sendBytes = sendto(brFd,buffer,strlen(buffer),0,
+            (struct sockaddr*)&theirAddr,sizeof(struct sockaddr)))== -1){
+        perror("br_exit:udp send msg failed!");
 	    return EXIT_FAILURE;
     }
     printf("%d\n",sendBytes);
