e332eb43dc02dd82d3451331b7edb61240af70b8
wql <18353184289@163.com>
2018-05-17 11:06:34 +0800

    1

--- user/user.c
+++ user/user.c
@@ -3,11 +3,18 @@
 static IPMSG_USER* ul_head_addr;
 static IPMSG_USER* ul_cur_addr;
 
+//userlist init
 void userlist_init(void){
     ul_head_addr = uListInit();
-    ul_cur_addr = ul_head_addr;
-    
+    ul_cur_addr = uListItemAdd(ul_head_addr,"wql","hehe",3);
+    userlist_print();
+    uListDelete(ul_head_addr);
 }
+//print all user
 void userlist_print(void){
-
+    IPMSG_USER* temp = ul_head_addr;
+    while(temp != NULL){
+        printf("%s\t%s\t\n",temp->name,temp->host);
+        temp = temp->next;
+    }
 }
--- user/user.h
+++ user/user.h
@@ -4,4 +4,6 @@
 #include <stdio.h>
 #include "../my-ds/ds-op.h"
 
+void userlist_init(void);
+void userlist_print(void);
 #endif
