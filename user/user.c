eb06449ff87757703ec44bdc7e4b64d01c05fafc
wql <18353184289@163.com>
2018-05-17 11:19:02 +0800

    1

--- user/user.c
+++ user/user.c
@@ -5,10 +5,10 @@ static IPMSG_USER* ul_cur_addr;
 
 //userlist init
 void userlist_init(void){
-    ul_head_addr = uListInit();
-    ul_cur_addr = uListItemAdd(ul_head_addr,"wql","hehe",3);
+    ul_head_addr = userlist_ds_init();
+    ul_cur_addr = userlist_item_add(ul_head_addr,"wql","hehe",3);
     userlist_print();
-    uListDelete(ul_head_addr);
+    userlist_ds_destory(ul_head_addr);
 }
 //print all user
 void userlist_print(void){
