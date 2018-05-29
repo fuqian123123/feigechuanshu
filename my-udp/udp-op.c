40f2fc545201b645bd81dc98ed0cf6910cd8ae02
wql <18353184289@163.com>
2018-05-29 16:58:03 +0800

    1

--- my-udp/udp-op.c
+++ my-udp/udp-op.c
@@ -2,8 +2,8 @@
 
 #define BR_ENTRY_FLAG 0
 #define BR_EXIT_FLAG 1
-static const char BR_ADDR[] = "10.22.255.255";
-//static const char BR_ADDR[] = "192.168.43.255";
+//static const char BR_ADDR[] = "10.22.255.255";
+static const char BR_ADDR[] = "192.168.43.255";
 static const int BR_PORT = 4001;
 static const int BR_RECV_PORT = 4001;
 static const int UNI_PORT = 4003;
